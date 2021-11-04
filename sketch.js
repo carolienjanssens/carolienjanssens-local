e77df7726d39fd3a65a9d8a3073aacde21284c85
Carolien Janssens <carolienjanssens@bluewin.ch>
2021-11-04 14:52:56 +0100

    initial commit of p5.js sketch

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(800, 1000);
+  background(255);
+  colorMode(HSB,100);
+}
+
+function draw() {
+  background(map(mouseX,0,width,60,8),100,map(mouseY,0,height,90,10));
+}
